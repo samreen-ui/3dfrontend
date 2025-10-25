95394a76b2a42ddd95d168916793a811b07c285e
samreen fatima <samreenf3112@gmail.com>
2025-10-26 01:21:41 +0530

    Fix syntax error in vite.config.js

--- webapp/vite.config.js
+++ webapp/vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
-  base:'/3dfrontend/'
+  plugins: [tailwindcss(), react()]s
 })
